b759f53b82be68481b65a7c1bd1a1c87a8319dd9
SrPrakhar <32195031+SrPrakhar@users.noreply.github.com>
2019-11-22 16:03:36 +0100

    Updated with http_to_gcs

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -3,6 +3,7 @@ from datetime import date, datetime, timedelta
 import airflow
 from airflow.models import DAG
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+from airflow_training.operators import HttpToGcsOperator
 
 args = {
 "owner": "Prakhar",
@@ -21,5 +22,17 @@ pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
   bucket="airflow-training-data-11",
   filename="{{ ds }}/properties_{}.json",
   postgres_conn_id="Postgres_Cnxn",
+  dag=dag,
+)
+
+
+http_to_gcs = HttpToGcsOperator(
+  task_id="get_currency_" + currency,
+  method="GET",
+  endpoint=f"/history?start_at={{yesterday_ds}}&end_at={{ds}}&symbols={currency}&base=GBP",
+  #http_conn_id="airflow-training-currency-http",
+  http_conn_id="http-default",
+  gcs_path="currency/{{ ds }}-" + currency + ".json",
+  gcs_bucket="airflow-training-data-11",
   dag=dag,
 )
