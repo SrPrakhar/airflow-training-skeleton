415918a970d978254ecb028e1361902e80de6436
SrPrakhar <32195031+SrPrakhar@users.noreply.github.com>
2019-11-22 11:04:10 +0100

    Draft Exercise 2

---
+++ dags/excercise2.py
@@ -0,0 +1,45 @@
+from datetime import date
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+args = {
+    'owner': 'Prakhar',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='excercise2',
+    default_args=args,
+    schedule_interval="@once",
+)
+
+def print_execution_date(**context):
+    today = date.today()
+    print("Today's date:", today)
+
+# [START howto_operator_bash]
+c = PythonOperator(
+    task_id="print_execution_date",
+    python_callable=print_execution_date,
+    provide_context=True,
+    dag=dag,
+)
+# [END howto_operator_bash]
+
+
+
+wait_5 = BashOperator(task_id="wait_5", bash_command="sleep 5", dag=dag)
+
+wait_1 = BashOperator(task_id="wait_1", bash_command="sleep 1", dag=dag)
+
+wait_10 = BashOperator(task_id="wait_10", bash_command="sleep 10", dag=dag)
+
+the_end = DummyOperator(task_id="the_end", dag=dag)
+
+print_execution_date >> [wait_5, wait_1, wait_10]
+
+[wait_5, wait_1, wait_10] >> the_end
