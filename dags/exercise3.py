acce96d7d4ae98309f966495b38ccefe68a1bb48
SrPrakhar <32195031+SrPrakhar@users.noreply.github.com>
2019-11-22 13:16:32 +0100

    draftVersion

---
+++ dags/exercise3.py
@@ -0,0 +1,34 @@
+from datetime import date, datetime, timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+args = {
+    'owner': 'Prakhar',
+    #'start_date': datetime(2019, 11, 17),
+}
+
+dag = DAG(
+    dag_id='excercise3',
+    default_args=args,
+    schedule_interval="@daily",
+)
+
+
+
+def _get_weekday(execution_date, **context):
+  return execution_date.strftime("%a")
+
+branching = BranchPythonOperator(
+  task_id="branching", 
+  python_callable=_get_weekday, 
+  provide_context=True, 
+  dag=dag
+)
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+for day in days:
+branching >> DummyOperator(task_id=day, dag=dag)
